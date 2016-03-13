db28edcfc3947b4ac60d65de0f4fa26ef3bbc91f
deuteronx <rawipas.u@gmail.com>
2016-03-13 00:38:38 +0000

    Remove "Browse" button from index page

--- action_dir.php
+++
@@ -1,6 +0,0 @@
-<?php
-$query = get_http_query(array('dir' => $dirname));
-$browse_url = "browse.php?$query";
-?>
-
-<a href="<?php echo $browse_url ?>" class="btn btn-xs btn-success glyphicon glyphicon-folder-open" style="width:40px;"></a>
--- index.php
+++ index.php
@@ -15,25 +15,21 @@ $dirlist_count = count($dirlist);
 	  <thead>
 		<tr>
 		  <th>Directory</th>
-		  <th>Action</th>
 		</tr>
 	  </thead>
 	  <tbody>
 		<?php
 			if ($dirlist_count > 0) {
 				foreach ($dirlist as $dirname) {
+					$query = get_http_query(array('dir' => $dirname));
 					echo "<tr>";
-					echo "<td>$dirname</td>";
-					echo "<td>";
-					require 'action_dir.php';
-					echo "</td>";
+					echo "<td><a href=\"browse.php?$query\">$dirname</a></td>";
 					echo "</tr>";
 				}
 			}
 			else {
 				echo "<tr>";
 				echo "<td>Please add files to the directory.</td>";
-				echo "<td></td>";
 				echo "</tr>";
 			}	
 		?>
